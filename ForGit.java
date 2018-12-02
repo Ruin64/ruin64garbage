b2c6489ef659099a4ae91eb1943814be475fa59d
Ruin64 <ruin6488@gmail.com>
2018-12-02 16:34:36 +0400

    and one more hello

--- ForGit.java
+++ ForGit.java
@@ -2,5 +2,6 @@ class ForGit{
     public static void main(String[] args) {
         System.out.println("Hello git");
 		System.out.println("Hello again");
+		System.out.println("And again");
     }
 }
