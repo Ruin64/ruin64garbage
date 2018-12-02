5b3a489fe198f853cc0eb3c193419222e6526686
Ruin64 <ruin6488@gmail.com>
2018-12-02 15:46:56 +0400

    sout cycling

--- ForGit.java
+++ ForGit.java
@@ -1,5 +1,7 @@
 class ForGit{
     public static void main(String[] args) {
-        System.out.println("Hello git");
+	for(int i = 0; i < 10: i++){
+        	System.out.println("Hello git");
+	}
     }
 }
