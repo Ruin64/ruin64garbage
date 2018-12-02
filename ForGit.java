827160e0b2cab3a5852efc3b9b5c7f6027719b2b
Ruin64 <ruin6488@gmail.com>
2018-12-02 16:32:44 +0400

    changes in hello string with ";"

--- ForGit.java
+++ ForGit.java
@@ -1,7 +1,7 @@
 class ForGit{
     public static void main(String[] args) {
 	for(int i = 0; i < 8: i++){
-        	System.out.println("Hello git");
+        	System.out.println("Hello git;");
 			
 	}
     }
